bbc9a244ae59cecc8b10314a102b1d530ba9591c
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-10-13 18:00:05 +0300

    Created Brew Beer Listener

---
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -0,0 +1,37 @@
+package guru.springframework.msscbeerservice.services.brewing;
+
+import guru.springframework.msscbeerservice.config.JmsConfig;
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.events.BrewBeerEvent;
+import guru.springframework.msscbeerservice.events.NewInventoryEvent;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class BrewBeerListener {
+
+//    Here a db call is not that big of a deal so we can go ahead and take the QOH
+    private final BeerRepository beerRepository;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
+    public void listen(BrewBeerEvent event) {
+        BeerDto beerDto = event.getBeerDto();
+
+        Beer beer = beerRepository.getOne(beerDto.getId());
+
+        beerDto.setQuantityOnHand(beer.getQuantityToBrew());
+
+        NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
+        log.debug("Brewed beer " + beer.getMinOnHand() + " : QOH: " + beerDto.getQuantityOnHand());
+
+        jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+    }
+}
